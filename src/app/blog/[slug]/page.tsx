50e7431a8860bf5e0ef05e7365cfbe795c444025
Haseeb Asad <haseebasad@mac.home>
2025-01-25 13:33:42 +0000

    simplify slug until we add posts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,5 @@
 // app/blog/[slug]/page.tsx
-type Props = {
-    params: {
-      slug: string
-    }
-  }
-  
-  // src/app/blog/[slug]/page.tsx
-// If you're not using params, you can remove it:
-export default function BlogPost() {  // Remove {params} if not used
+export default function BlogPost() {
   return (
     <div>
       {/* Your component content */}
