7ce77d4ea1eb68453d200707b7801e271030fce1
Haseeb Asad <89456132+HaseebAsad@users.noreply.github.com>
2025-07-17 13:59:40 +0100

    Batch 2025-07-17

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -11,6 +11,217 @@ export interface Photo {
   
   export const photos: Photo[] = [
     {
+      id: '42',
+      src: '/images/gallery/bruce-lee-sf.jpeg',
+      alt: 'Bruce Lee mural, Chinatown, San Francisco',
+      width: 4284,
+      height: 5712,
+      category: 'iphone 15 Pro',
+      date: '2025-07-12'
+    },
+    
+    {
+      id: '41',
+      src: '/images/gallery/palace-of-fine-arts.jpeg',
+      alt: 'Palace of Fine Arts, San Francisco',
+      width: 3024,
+      height: 4032,
+      category: 'iphone 15 Pro',
+      date: '2025-07-11'
+    },
+
+    {
+      id: '40',
+      src: '/images/gallery/karate-kid-bs.jpeg',
+      alt: 'Big Sur',
+      width: 4000,
+      height: 6000,
+      category: 'FujiFilm XF10',
+      date: '2025-07-11'
+    },
+
+    {
+      id: '39',
+      src: '/images/gallery/big-sur.jpeg',
+      alt: 'Drive to Big Sur',
+      width: 4284,
+      height: 5712,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-11'
+    },
+
+    {
+      id: '38',
+      src: '/images/gallery/gotham-sf.jpeg',
+      alt: 'San Francisco',
+      width: 3024,
+      height: 4032,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-10'
+    },
+
+    {
+      id: '37',
+      src: '/images/gallery/secret-beach.jpeg',
+      alt: 'Secret Beach, Lake Tahoe',
+      width: 3024,
+      height: 4032,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-09'
+    },
+
+    {
+      id: '36',
+      src: '/images/gallery/glacier-point.jpeg',
+      alt: 'Star gazing at Glacier Point, Yosemite',
+      width: 3024,
+      height: 4032,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-08'
+    },
+
+    {
+      id: '35',
+      src: '/images/gallery/vernal-falls.jpeg',
+      alt: 'Vernal Falls, Yosemite',
+      width: 4000,
+      height: 6000,
+      category: 'FujiFilm XF10',
+      date: '2025-07-08'
+    },
+
+    {
+      id: '34',
+      src: '/images/gallery/supra-yosemite.jpeg',
+      alt: 'Road to Yosemite',
+      width: 3024,
+      height: 4032,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-08'
+    },
+
+    {
+      id: '33',
+      src: '/images/gallery/death-valley.jpeg',
+      alt: 'Signs of Life, Death Valley',
+      width: 3024,
+      height: 4032,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-07'
+    },
+
+    {
+      id: '32',
+      src: '/images/gallery/fremont-street.jpeg',
+      alt: 'Fremont Street, Las Vegas',
+      width: 4284,
+      height: 5712,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-06'
+    },
+
+    {
+      id: '31',
+      src: '/images/gallery/las-vegas.jpeg',
+      alt: 'Las Vegas',
+      width: 3024,
+      height: 4032,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-05'
+    },
+
+    {
+      id: '30',
+      src: '/images/gallery/zion.jpeg',
+      alt: 'Zion National Park',
+      width: 4000,
+      height: 6000,
+      category: 'FujiFilm XF10',
+      date: '2025-07-04'
+    },
+
+    {
+      id: '29',
+      src: '/images/gallery/4th-july.jpeg',
+      alt: '4th of July Parade, Zion National Park',
+      width: 3098,
+      height: 4131,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-04'
+    },
+    
+    {
+      id: '28',
+      src: '/images/gallery/grand-canyon2.jpeg',
+      alt: 'Exploring the Grand Canyon',
+      width: 3024,
+      height: 4032,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-03'
+    },
+
+    {
+      id: '27',
+      src: '/images/gallery/grand-canyon.jpeg',
+      alt: 'Chasing the sunset, Grand Canyon',
+      width: 6000,
+      height: 4000,
+      category: 'FujiFilm XF10',
+      date: '2025-07-03'
+    },
+
+    {
+      id: '26',
+      src: '/images/gallery/shohei-ohtani.jpeg',
+      alt: 'A star in his home, Dodger Stadium',
+      width: 3024,
+      height: 4032,
+      category: 'iPhone 15 Pro',
+      date: '2025-07-01'
+    },
+
+    {
+      id: '25',
+      src: '/images/gallery/the-getty.jpeg',
+      alt: 'The Getty Museum',
+      width: 4000,
+      height: 6000,
+      category: 'FujiFilm XF10',
+      date: '2025-06-29'
+    },
+
+    {
+      id: '24',
+      src: '/images/gallery/santa-monica-pier.jpeg',
+      alt: 'Ocean mist, Santa Monica Pier',
+      width: 6000,
+      height: 4000,
+      category: 'FujiFilm XF10',
+      date: '2025-06-29'
+    },
+
+    {
+      id: '23',
+      src: '/images/gallery/muscle-beach.jpeg',
+      alt: 'Muscle Beach, Santa Monica',
+      width: 4000,
+      height: 6000,
+      category: 'FujiFilm XF10',
+      date: '2025-06-29'
+    },
+
+    {
+      id: '22',
+      src: '/images/gallery/griffith-observatory.jpeg',
+      alt: 'Griffith Observatory',
+      width: 4000,
+      height: 6000,
+      category: 'FujiFilm XF10',
+      date: '2025-06-28'
+    },
+
+
+    {
       id: '21',
       src: '/images/gallery/paris-gardens.jpeg',
       alt: 'Paris',
